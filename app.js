f8b2e5bade300ccc486c3bbf8236ed981018449f
LINSHEN WU <linshenwu@LINSHENs-MacBook-Pro.local>
2016-08-31 22:35:54 -0400

    Initial Commit

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var express = require('express');
 var cityRouter = require('./public/routes/city_mongo');
 var app = express();
 
+app.set('port', (process.env.PORT || 8000));
+
 
 app.use(express.static('public'));
-//app.use('/cities', cityRouter);
+app.use('/cities', cityRouter);
 
-app.listen(3000, ()=>{console.log("Online Now at 3000");});
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
