3dea8ce31bcea1735f8d84291a6d195ed5c86816
LINSHEN WU <linshenwu@LINSHENs-MacBook-Pro.local>
2016-09-14 23:06:45 -0400

    Added Cors

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var cityRouter = require('./public/routes/city_mongo');
 var app = express();
+var cors = require('cors')
+
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.set('port', (process.env.PORT || 8000));
 
---
+++ public/registerForm.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Register extends React.Component {
+  render(){
+    return <div> hi </div>
+  }
+};
+
+ReactDOM.render(<Register/>, document.getElementById('registerForm'))
